db262a50064d0ee8ab7f23f0d633518c72d1b3d5
JadiellyL <m.jadiellydlima@gmail.com>
2023-08-12 12:46:15 -0300

    Exercicio custo usar alcool ou gasolina

---
+++ src/exercicios/custoAlcollOuGasolina.java
@@ -0,0 +1,26 @@
+package exercicios;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class custoAlcollOuGasolina {
+
+	public static void main(String[] args) {
+		double valMaxAlcool, gasolina, alcool;
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Valor da gasolina");
+		gasolina = teclado.nextDouble();
+		System.out.println("Valor do alcool");
+		alcool = teclado.nextDouble();
+		
+		valMaxAlcool =(70*gasolina)/100;
+		if(alcool < valMaxAlcool) {
+			System.out.println("Usar alcool");
+		}else {
+			System.out.println("Usar gasolina");
+		}
+		teclado.close();
+	}
+	
+}
